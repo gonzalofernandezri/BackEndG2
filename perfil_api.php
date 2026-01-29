9886c44ae3906583ea2463fac69e1d9d50e1fd53
David <orueta.david@gmail.com>
2026-01-29 15:51:47 +0100

    cambios perfil

---
+++ perfil_api.php
@@ -0,0 +1,43 @@
+
+<?php
+
+require_once("usuarios.php");
+
+
+session_start();
+
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *"); // solo para pruebas
+header("Access-Control-Allow-Credentials: true"); // permite cookies
+
+if(!isset($_SESSION["user_id"])) {
+echo json_encode(['error' => "usuario no encontrado"]);
+    
+exit;
+}
+
+
+conexionBBDD();
+$conn = conexionBBDD();
+
+
+
+
+
+$user_id= $_SESSION["user_id"];
+
+$sql = "SELECT id, username, email, role from users where id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+
+ echo json_encode($user);
+ ?>
+
+
+
+
+
+
